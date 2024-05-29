4f117f75a7256c5ef016c4ee0af07023aaabf801
xpakx <55586470+xpakx@users.noreply.github.com>
2024-05-29 15:51:25 +0200

    feat(frontend): create menu component

--- frontend/src/app/main/menu/menu.component.ts
+++ frontend/src/app/main/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ListType } from '../dto/list-type';
+import { GameType } from '../dto/game-type';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  requestView: boolean = false;
+  activeView: boolean = false;
+  error: boolean = false;
+  errorMsg: String = "";
+
+  @Output() openGame: EventEmitter<number> = new EventEmitter<number>();
+  @Output() openGameModal: EventEmitter<GameType> = new EventEmitter<GameType>();
+  @Output("get") getList: EventEmitter<ListType> = new EventEmitter<ListType>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getRequests() {
+    this.getList.emit("requests");
+  }
+
+  getGames() {
+    this.getList.emit("games");
+  }
+
+  getArchive() {
+    this.getList.emit("archive");
+  }
+
+  newGame() {
+    this.openGameModal.emit("User");
+  }
+
+  newAIGame() {
+    this.openGameModal.emit("AI");
+  }
+
+  open(gameId: number) {
+    this.openGame.emit(gameId);
+  }
+
+  logout() {
+    localStorage.removeItem('refresh');
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+  }
 }
