45b1375f2a07328ab692bd1b9ea36ef7716c69b0
xpakx <55586470+xpakx@users.noreply.github.com>
2024-06-06 21:04:36 +0200

    feat(game): let user send chat messages

---
+++ game/src/main/java/io/github/xpakx/chess/game/GameController.java
@@ -0,0 +1,31 @@
+package io.github.xpakx.chess.game;
+
+import io.github.xpakx.chess.game.dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class GameController {
+    private final GameService service;
+
+    @MessageMapping("/chat/{id}")
+    @SendTo("/topic/chat/{id}")
+    public ChatMessage chat(@DestinationVariable Long id, ChatRequest request, Principal principal) {
+        var msg = new ChatMessage();
+        var username = "guest";
+        if (principal != null && principal.getName() != null) {
+            username = principal.getName();
+
+        }
+        msg.setPlayer(username);
+        msg.setMessage(request.getMessage());
+        return msg;
+    }
+}
---
+++ game/src/main/java/io/github/xpakx/chess/game/dto/ChatMessage.java
@@ -0,0 +1,11 @@
+package io.github.xpakx.chess.game.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private String player;
+    private String message;
+}
