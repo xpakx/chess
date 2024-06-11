bbdb31828e1a8576ca8f0decde9fbaabb7d7d714
xpakx <55586470+xpakx@users.noreply.github.com>
2024-06-11 16:56:27 +0200

    chore(engine): declare exchanges and queues

--- game-engine/src/rabbit/mod.rs
+++ game-engine/src/rabbit/mod.rs
@@ -1,4 +1,11 @@
 use std::time::Duration;
+use lapin::{options::{BasicConsumeOptions, ExchangeDeclareOptions, QueueBindOptions, QueueDeclareOptions}, types::FieldTable, ExchangeKind};
+
+const EXCHANGE_NAME: &str = "chess.moves.topic";
+const MOVES_QUEUE: &str = "chess.moves.queue";
+const AI_QUEUE: &str = "chess.moves.ai.queue";
+
+pub const DESTINATION_EXCHANGE: &str = "chess.engine.topic";
 
 pub async fn lapin_listen(pool: deadpool_lapin::Pool) {
     let mut retry_interval = tokio::time::interval(Duration::from_secs(5));
@@ -20,7 +27,74 @@ async fn init_lapin_listen(pool: deadpool_lapin::Pool) -> Result<(), Box<dyn std
     })?;
     let channel = rmq_con.create_channel().await?;
 
-    // TODO
+    channel.queue_declare(
+        MOVES_QUEUE,
+        QueueDeclareOptions::default(),
+        Default::default(),
+        )
+        .await
+        .expect("Cannot declare queue");
+
+    channel
+        .queue_bind(
+            MOVES_QUEUE,
+            EXCHANGE_NAME,
+            "move",
+            QueueBindOptions::default(),
+            FieldTable::default(),
+            )
+        .await
+        .expect("Cannot bind queue");
+
+    channel.queue_declare(
+        AI_QUEUE,
+        QueueDeclareOptions::default(),
+        Default::default(),
+        )
+        .await
+        .expect("Cannot declare queue");
+
+    channel
+        .queue_bind(
+            AI_QUEUE,
+            EXCHANGE_NAME,
+            "ai",
+            QueueBindOptions::default(),
+            FieldTable::default(),
+            )
+        .await
+        .expect("Cannot bind queue");
+
+    channel
+        .exchange_declare(
+            DESTINATION_EXCHANGE,
+            ExchangeKind::Topic,
+            ExchangeDeclareOptions {
+                durable: true,
+                ..Default::default()
+            },
+            FieldTable::default(),
+            )
+        .await
+        .expect("Cannot declare exchange");
+
+    let _move_consumer = channel.basic_consume(
+        MOVES_QUEUE,
+        "engine_move_consumer",
+        BasicConsumeOptions::default(),
+        FieldTable::default())
+        .await
+        .expect("Cannot create consumer");
+
+    let _ai_consumer = channel.basic_consume(
+        AI_QUEUE,
+        "engine_ai_consumer",
+        BasicConsumeOptions::default(),
+        FieldTable::default())
+        .await
+        .expect("Cannot create consumer");
+
+    // TODO: declare delegates for consumers
 
     let mut test_interval = tokio::time::interval(Duration::from_secs(5));
     loop {
