37aa8a17ca37e19e6e24d4209a644ffe934921ac
xpakx <55586470+xpakx@users.noreply.github.com>
2024-06-20 18:15:19 +0200

    feat(frontend): generate correct moves from rook and king during castling

--- frontend/src/app/board/board/board.component.ts
+++ frontend/src/app/board/board/board.component.ts
@@ -357,18 +357,29 @@ export class BoardComponent implements OnInit, OnDestroy {
     const isCheck = check ? true : false;
     const isMate = check && check == "#" ? true : false;
 
-    const rookStart = [0, 0];
-    const rookTarget = [0, 0];
-    const kingStart = [0, 0];
-    const kingTarget = [0, 0];
+    if (color == "White" && !isLongCastle) {
+      return this.generateCastling([7, 7], [7, 5], [7, 4], [7, 6], isCheck, isMate);
+    }
+    if (color == "White" && isLongCastle) {
+      return this.generateCastling([7, 0], [7, 3], [7, 4], [7, 2], isCheck, isMate);
+    }
+    if (color == "Black" && !isLongCastle) {
+      return this.generateCastling([0, 7], [0, 5], [0, 4], [0, 6], isCheck, isMate);
+    }
+    if (color == "Black" && isLongCastle) {
+      return this.generateCastling([0, 0], [0, 3], [0, 4], [0, 3], isCheck, isMate);
+    }
 
+    return undefined;
+  }
 
+  generateCastling(rookStart: number[], rookTarget: number[], kingStart: number[], kingTarget: number[], check: boolean, mate: boolean): Castling {
     return {
       kingMove: {
         piece: "King",
         capture: false,
-        check: isCheck,
-        mate: isMate,
+        check: check,
+        mate: mate,
         enpassant: false,
         start: kingStart,
         target: kingTarget,
@@ -376,13 +387,12 @@ export class BoardComponent implements OnInit, OnDestroy {
       rookMove: {
         piece: "Rook",
         capture: false,
-        check: isCheck,
-        mate: isMate,
+        check: check,
+        mate: mate,
         enpassant: false,
         start: rookStart,
         target: rookTarget,
       },
     };
-
   }
 }
