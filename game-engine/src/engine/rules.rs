a39fe1ba5c445ff12aac789a89db8dabdafc63c1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-06-25 21:09:55 +0200

    feat(engine): transform all knight moves to move list

--- game-engine/src/engine/rules.rs
+++ game-engine/src/engine/rules.rs
@@ -6,11 +6,40 @@ const NOT_A_FILE: u64 =  0b01111111_01111111_01111111_01111111_01111111_01111111
 const NOT_AB_FILE: u64 = 0b00111111_00111111_00111111_00111111_00111111_00111111_00111111_00111111;
 const NOT_H_FILE: u64 =  0b11111110_11111110_11111110_11111110_11111110_11111110_11111110_11111110;
 const NOT_GH_FILE: u64 = 0b11111100_11111100_11111100_11111100_11111100_11111100_11111100_11111100;
-const RANK_4: u64 = 0b00000000_00000000_00000000_00000000_11111111_00000000_00000000_00000000;
-const RANK_5: u64 = 0b00000000_00000000_00000000_11111111_00000000_00000000_00000000_00000000;
+const RANK_4: u64 =      0b00000000_00000000_00000000_00000000_11111111_00000000_00000000_00000000;
+const RANK_5: u64 =      0b00000000_00000000_00000000_11111111_00000000_00000000_00000000_00000000;
 
-pub fn get_possible_moves(board: &BitBoard, color: &Color) -> Vec<u64> {
-    vec![]
+pub struct Move {
+    from: u8,
+    to: u8,
+    promotion: bool,
+    capture: bool,
+    castling: bool,
+}
+
+pub fn get_possible_moves(board: &BitBoard, color: &Color) -> Vec<Move> {
+    let (pawns, knights, bishops, rooks, queens, kings, enemy) = match color {
+        Color::Red => (board.black_pawns, board.black_knights, board.black_bishops, board.black_rooks, board.black_queens, board.black_king, 
+                       board.white_pawns | board.white_knights | board.white_bishops | board.white_rooks | board.white_queens | board.white_king),
+        Color::White => (board.white_pawns, board.white_knights, board.white_bishops, board.white_rooks, board.white_queens, board.white_king,
+                       board.black_pawns | board.black_knights | board.black_bishops | board.black_rooks | board.black_queens | board.black_king),
+    };
+    let mut result = Vec::new();
+    let mut current = knights;
+    let all_pieces = pawns | knights | bishops | rooks | queens | kings | enemy;
+    let empty = !all_pieces;
+    while current != 0 {
+        let from = current.trailing_zeros() as u8;
+        let knight = 1 << from;
+        current = current & !knight;
+        let mut moves = get_knight_moves(&knight, &(empty | enemy));
+        while moves != 0 {
+            let to = moves.trailing_zeros() as u8;
+            moves = moves & !(1 << to);
+            result.push(Move { from, to, promotion: false, capture: false, castling: false }); // TODO
+        }
+    }
+    result
 }
 
 pub fn get_knight_moves(knight: &u64, targets: &u64) -> u64 {
